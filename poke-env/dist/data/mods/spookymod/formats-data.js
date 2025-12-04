dc94f4dc74dd5ce86f0ee64fee03984b0cc63896
NatePGroves <natepgroves@gmail.com>
2025-12-04 13:29:35 -0500

    removed gitignore so we can hopefully actually run the server

---
+++ poke-env/dist/data/mods/spookymod/formats-data.js
@@ -0,0 +1,286 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+var formats_data_exports = {};
+__export(formats_data_exports, {
+  FormatsData: () => FormatsData
+});
+module.exports = __toCommonJS(formats_data_exports);
+const FormatsData = {
+  aegislash: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  banettemega: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  ceruledge: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  dofagrigus: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  eddyazbear: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  fluttermane: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  fluttermane2: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  gourgeisttiny: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  gourgeist: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  gourgeistlarge: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  gourgeistgigantic: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  houndstone: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  indeedee: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  jableye: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  jobapplication: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  lanturnevil: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  merasmus: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  nursola: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  okestarspirit: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  ppiritomb: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  quagsiredead: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  rotom: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  shedinja: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  trevenant: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  ursalunabloodmoon: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  kilotic: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  vampharos: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  wunala: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  xorygonz: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  yveltal: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  zoroarkhisui: {
+    tier: "OU",
+    doublesTier: "DOU"
+  },
+  aegislashshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  banettemegashrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  ceruledgeshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  dofagrigusshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  eddyazbearshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  fluttermaneshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  fluttermane2shrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeisttinyshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeistshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeistlargeshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeistgiganticshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  houndstoneshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  indeedeeshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  jableyeshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  jobapplicationshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  lanturnevilshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  merasmusshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  nursolashrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  okestarspiritshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  ppiritombshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  quagsiredeadshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  rotomshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  shedinjashrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  trevenantshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  ursalunabloodmoonshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  kiloticshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  vampharosshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  wunalashrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  xorygonzshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  yveltalshrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  zoroarkhisuishrunken: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeistsmall: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  gourgeistsuper: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  },
+  trevenantrevenant: {
+    tier: "Illegal",
+    doublesTier: "Illegal"
+  }
+};
+//# sourceMappingURL=formats-data.js.map
